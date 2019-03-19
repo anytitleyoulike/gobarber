9afa9552324e3c1d3cd460c1e2124167cd44ad1f
Marcello Henrique <maarc.hen@gmail.com>
2019-03-19 15:05:20 -0400

    logout and auth middlewares

--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,22 @@ const upload = require('multer')(multerConfig);
 const routes = express.Router();
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
+const authMiddleware = require('../src/app/middlewares/auth');
+const guestMiddleware = require('../src/app/middlewares/guest');
 
-routes.get('/', SessionController.create);
+routes.use('/app', authMiddleware);
+
+routes.get('/', guestMiddleware, SessionController.create);
 routes.post('/signin', SessionController.store);
 
-routes.get('/signup', UserController.create);
+routes.get('/signup', guestMiddleware, UserController.create);
 routes.post('/signup', upload.single('avatar'), UserController.store);
 
 routes.get('/app/dashboard', (req, res) => {
-  res.render('dashboard');
+  console.log(req.session.user);
+  return res.render('dashboard');
 });
+
+routes.get('/app/logout', SessionController.destroy);
 
 module.exports = routes;
